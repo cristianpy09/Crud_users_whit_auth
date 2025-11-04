a1d16c4d9d976da58592af7031fb46c855a7afb5
cristianpy09 <cristiancelismoreno@gmail.com>
2025-11-04 07:20:26 -0500

    change next version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
-import { Theme } from "@radix-ui/themes";
+
 import Footer from "@/components/global/Footer";
 
 const geistSans = Geist({
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme appearance="dark"> {children}</Theme>
+        {children}
 
         <Footer />
       </body>
